60f729af0bb9313ca075fbb1ebb52f865fdec8a6
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-05-15 17:08:45 -0700

    initial implementation

---
+++ test/create-host-label.unit.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var expect = require('code').expect;
+var describe = lab.describe;
+var it = lab.test;
+var beforeEach = lab.beforeEach;
+var createHostLabel = require('../lib/create-host-label.js');
+
+describe('create host label', function () {
+  var ctx;
+  beforeEach(function (done) {
+    ctx = {};
+    done();
+  });
+
+  it('should create a host label', function (done) {
+    var label = createHostLabel('{shortHash}-{repoName}-staging-{ownerUsername}', [
+      {
+        key: 'ownerUsername',
+        val: 'ownerUsername',
+        max: 39 // maxlength of username in github is 39
+      },
+      {
+        key: 'repoName',
+        val: 'repoName',
+        min: 'repoName'.length
+      },
+      {
+        key: 'shortHash',
+        val: 'abcdef',
+        min: 6
+      }
+    ]);
+
+    expect(label).to.equal('abcdef-repoName-staging-ownerUsername');
+    done();
+  });
+});
